aa4b25302e786be365849f2d366115a3c0a1dda8
David Hildebrandt <davihildebran@gmail.com>
2021-10-03 22:50:37 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'learning-angular-events';
+  evenList: number[] = [];
+  oddList: number[] = [];
+
+  onStartGame(number: number) {
+    if (number % 2 === 0) {
+      this.evenList.push(number);
+    }
+    else {
+      this.oddList.push(number);
+    }
+  }
+
+  onEndGame() {
+    
+  }
+
 }
